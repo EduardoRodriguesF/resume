b53ad2533c0c25427971c12c425579703d68c467
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-01-18 08:35:37 -0300

    feat: new education timeline

--- components/EducationPassage/index.js
+++ components/EducationPassage/index.js
@@ -1,23 +1,18 @@
 import Image from 'next/image';
-import { Container, Timeline, Line, Circle, Content, Details } from './styles';
+import { Fade } from "react-awesome-reveal";
+
+import { Container } from './styles';
 
 export default function EducationPassage({ children, title, date, done, src, alt }) {
   return (
-    <Container>
-      <Timeline>
-        <Line done={done}>
-          <hr/>
-          <Circle done={done} />
-        </Line>
-      </Timeline>
-      <Content>
-        <Image layout="fixed" width="200" height="200" src={src} alt={alt} />  
-        <Details>
+    <Container> 
+      <Fade triggerOnce direction="down">
+        <div>
           <h2>{title}</h2>
           <span>{date}</span>
           <p>{children}</p>
-        </Details>
-      </Content>
+        </div>
+      </Fade>
     </Container>
   );
 }
--- styles/pages/education.js
+++ styles/pages/education.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 
-export const Content = styled.div`
-  width: 100%;
-  padding: 92px 24px;
+export const Content = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 0 64px;
+  padding: 0;
+
+  ul { 
+    margin: 0;
+  }
+
+  @media screen and (max-width: 760px) {
+    display: block;
+  }
 `;

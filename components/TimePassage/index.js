61d7374830a77707d54a269a3712ddd2fc2e9207
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-01-29 09:21:08 -0300

    chore: time passage more vague so it can be customized for experience page

--- components/TimePassage/index.js
+++ components/TimePassage/index.js
@@ -3,14 +3,12 @@ import { Fade } from "react-awesome-reveal";
 
 import { Container } from './styles';
 
-export default function EducationPassage({ children, title, date, done }) {
+export default function TimePassage({ children, done }) {
   return (
     <Container done={done}> 
       <Fade triggerOnce direction="down">
         <div>
-          <h2>{title}</h2>
-          <span>{date}</span>
-          <p>{children}</p>
+          {children}
         </div>
       </Fade>
     </Container>
--- pages/education.js
+++ pages/education.js
@@ -4,7 +4,7 @@ import { FiAward } from 'react-icons/fi';
 
 import Layout from '../components/Layout';
 import GoBackButton from '../components/GoBackButton';
-import EducationPassage from '../components/EducationPassage';
+import TimePassage from '../components/TimePassage';
 
 import { Content } from '../styles/pages/education';
 
@@ -12,31 +12,43 @@ export default function Education() {
   return (
     <Layout>
       <Content>
-        <EducationPassage title="Webdesign course" date="2018 – 2019" done>
-          This was not my first contact with programming, but represents the time I decided to take it seriously as my <b>career</b>. 
-          Although being mainly fundamentals on <b>HTML</b> and <b>CSS</b>, it also taught me about <b>Adobe Photoshop</b> and <b>Google Webdesign</b> (besides I don't use it daily).<br /><br />
-          But as I was studying more advanced subjects alongside the course, what I really engaged to in it was my teacher's stories and tips on his main job as front-end on Carrefour.
-        </EducationPassage>
-        <EducationPassage title="Fullstack course" date={2020} done>
-          As I continued to study more and more, exploring all sorts of courses via Udemy and other websites like that, I ended up participating on <b>Next Level Week #3</b>, where I developed a simple orphanage finding page called <a href="https://github.com/EduardoRodriguesF/happy">Happy</a>.
-          After that, I got interested in the paid course Gostack, where I have been studying until some weeks ago and learned a lot about: <br /> 
-          <ul>
-            <li>TypeScript</li>
-            <li>Advanced JavaScript</li>
-            <li>ReactJS</li>
-            <li>React Native</li>
-            <li>Node.js</li>
-            <li>Amazon AWS and S3</li>
-            <li>ESLint/Prettier for code patterns</li>
-          </ul>
-          <a href="https://github.com/EduardoRodriguesF/gobarber">GoBarber</a>, the app developed thoughout the course, is a full application on web and mobile, so the list goes on and on.
-          The bootcamp covered all the way from scratch to deploy, giving me the knowledge required to work on real life services and provide maintence.<br/><br />
-          Not stopping there, the course is constantly getting updates. Rocketseat announced a revamped course, <b>Ignite</b>, in <b>March 2021</b>, which I am already excited to take.
-          <br/><br/><FiAward size={16} /> <Link href="/gostack-certificate.pdf">Certificate</Link>
-        </EducationPassage>
-        <EducationPassage title="Information Systems (Bachelor)" date="2021 – 2025">
-          Starting on February 5, I will be studying at <b>Unisanta</b> University. 
-        </EducationPassage>
+        <TimePassage done>
+          <h2>Webdesign course</h2>
+          <span>2018 – 2019</span>
+          <p>
+            This was not my first contact with programming, but represents the time I decided to take it seriously as my <b>career</b>. 
+            Although being mainly fundamentals on <b>HTML</b> and <b>CSS</b>, it also taught me about <b>Adobe Photoshop</b> and <b>Google Webdesign</b> (besides I don't use it daily).<br /><br />
+            But as I was studying more advanced subjects alongside the course, what I really engaged to in it was my teacher's stories and tips on his main job as front-end on Carrefour.
+          </p> 
+        </TimePassage>
+        <TimePassage done>
+          <h2>Fullstack course</h2>
+          <span>2020</span>
+          <p>
+            As I continued to study more and more, exploring all sorts of courses via Udemy and other websites like that, I ended up participating on <b>Next Level Week #3</b>, where I developed a simple orphanage finding page called <a href="https://github.com/EduardoRodriguesF/happy">Happy</a>.
+            After that, I got interested in the paid course Gostack, where I have been studying until some weeks ago and learned a lot about: <br /> 
+            <ul>
+              <li>TypeScript</li>
+              <li>Advanced JavaScript</li>
+              <li>ReactJS</li>
+              <li>React Native</li>
+              <li>Node.js</li>
+              <li>Amazon AWS and S3</li>
+              <li>ESLint/Prettier for code patterns</li>
+            </ul>
+            <a href="https://github.com/EduardoRodriguesF/gobarber">GoBarber</a>, the app developed thoughout the course, is a full application on web and mobile, so the list goes on and on.
+            The bootcamp covered all the way from scratch to deploy, giving me the knowledge required to work on real life services and provide maintence.<br/><br />
+            Not stopping there, the course is constantly getting updates. Rocketseat announced a revamped course, <b>Ignite</b>, in <b>March 2021</b>, which I am already excited to take.
+            <br/><br/><FiAward size={16} /> <Link href="/gostack-certificate.pdf">Certificate</Link>
+          </p>
+        </TimePassage>
+        <TimePassage title="Information Systems (Bachelor)" date="2021 – 2025">
+          <h2>Information Systems (Bachelor)</h2>
+          <span>2021 – 2025</span>
+          <p>
+            Starting on February 5, I will be studying at <b>Unisanta</b> University. 
+          </p>
+        </TimePassage>
       </Content>
     </Layout>
   );
